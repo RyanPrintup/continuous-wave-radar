59829ec80dfddf9abdef435490594d27644ff5ff
ryanprintup <ryan.printup@gmail.com>
2020-04-01 17:32:24 +0200

    Lock sampling to specific frequency

--- daq.c
+++ daq.c
@@ -1,19 +1,32 @@
-// Configuration
+/* Define Pinouts */
 const int RADAR_IN_PIN = A0;
+
+/* Serial Configuration */
 const int BAUD_RATE = 115200;
+
+/* Sampling Parameters */
+const int SAMPLING_FREQUENCY = 200; // Hz
+const int SAMPLING_PERIOD = ((1 / SAMPLING_FREQUENCY) * pow(10, 6)); // us
+unsigned long lastTime = 0;
 
 void setup()
 {
     Serial.begin(BAUD_RATE);
-
     pinMode(RADAR_IN_PIN, INPUT);
 } /* setup() */
 
 void loop()
 {
-    int value = digitalRead(RADAR_IN_PIN);
+    // Ensure sample frequency by checking time between
+    // samples and determing if we should sample the radar
+    if ((micros() - lastTime) >= SAMPLING_PERIOD)
+    {
+        lastTime = micros();
 
-    Serial.print(value);
+        // Sample radar and send over serial
+        int value = analogRead(RADAR_IN_PIN);
+        Serial.println(value);
+    }
 } /* loop() */
 
 /*** End of File ***/
